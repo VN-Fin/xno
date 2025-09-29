1b99d97206fc7c68e08477ad71dd10e4e5bb928a
kim <kimnt93@gmail.com>
2025-09-29 09:17:53 +0700

    Add default config

--- xno/config.py
+++ xno/config.py
@@ -5,44 +5,44 @@ import logging
 
 class AppConfig:
     # Postgresql config
-    _postgresql_host: str = os.environ['POSTGRES_HOST']
-    _postgresql_port: int = os.environ['POSTGRES_PORT']
-    _postgresql_db: str = os.environ['POSTGRES_DB']
-    _postgresql_user: str = os.environ['POSTGRES_USER']
-    _postgresql_password: str = os.environ['POSTGRES_PASSWORD']
+    postgresql_host: str = os.environ.get('POSTGRES_HOST', 'localhost')
+    postgresql_port: int = os.environ.get('POSTGRES_PORT', 5432)
+    postgresql_db: str = os.environ.get('POSTGRES_DB', 'xno')
+    postgresql_user: str = os.environ.get('POSTGRES_USER', 'xno')
+    postgresql_password: str = os.environ.get('POSTGRES_PASSWORD', 'xno_password')
     @property
     def postgresql_url(self):
         return "postgresql://{user}:{password}@{host}:{port}/{db}".format(
-            user=self._postgresql_user,
-            password=quote_plus(self._postgresql_password),
-            host=self._postgresql_host,
-            port=self._postgresql_port,
-            db=self._postgresql_db
+            user=self.postgresql_user,
+            password=quote_plus(self.postgresql_password),
+            host=self.postgresql_host,
+            port=self.postgresql_port,
+            db=self.postgresql_db
         )
 
     # Redis config
-    _redis_host: str = os.environ['REDIS_HOST']
-    _redis_port: int = os.environ['REDIS_PORT']
-    _redis_db: int = os.environ.get('REDIS_DB', 0)
+    redis_host: str = os.environ.get('REDIS_HOST', 'localhost')
+    redis_port: int = os.environ.get('REDIS_PORT', 6379)
+    redis_db: int = os.environ.get('REDIS_DB', 0)
     semaphore_key: str = "xno_data_semaphore"
     semaphore_max_permits: int = 5  # max 5 operations
     @property
     def redis_config(self):
         return {
-            'host': self._redis_host,
-            'port': self._redis_port,
-            'db': self._redis_db
+            'host': self.redis_host,
+            'port': self.redis_port,
+            'db': self.redis_db
         }
 
     # Kafka config
-    kafka_bootstrap_servers: str = os.environ['KAFKA_BOOTSTRAP_SERVERS']
+    kafka_bootstrap_servers: str = os.environ['KAFKA_SERVERS']
     kafka_topic: str = os.environ['KAFKA_TOPIC']
-    _kafka_default_group_id: str = 'xno-data-consumer-group'
+    kafka_default_group_id: str = 'xno-data-consumer-group'
     @property
     def kafka_consumer_config(self):
         return {
             'bootstrap.servers': self.kafka_bootstrap_servers,
-            'group.id': self._kafka_default_group_id,
+            'group.id': self.kafka_default_group_id,
             'auto.offset.reset': 'earliest'
         }
 

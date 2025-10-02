452b841d5576a19c8fa8cb3cea723d6f1806414c
kim <kimnt93@gmail.com>
2025-10-02 13:43:24 +0700

    remove examples

--- examples/connect_ohlcv.py
+++
@@ -1,9 +0,0 @@
-from xno.data import OhlcvDataManager
-
-if __name__ == "__main__":
-    OhlcvDataManager.get_instance("HPG", "m").load_data(
-        from_time="2023-01-01",
-        to_time="2023-10-01",
-    )
-    data = OhlcvDataManager.get_instance("HPG", "m").get_data()
-    print(data)

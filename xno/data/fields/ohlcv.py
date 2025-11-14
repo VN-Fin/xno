b641054f01ce289ad654e689318676243fb2a32d
kim <kimnt93@gmail.com>
2025-11-14 14:58:48 +0700

    remove Datamodel

--- xno/data/fields/ohlcv.py
+++ xno/data/fields/ohlcv.py
@@ -12,9 +12,9 @@ class Ohlcv:
     Volume = "Volume"
 
 
-class OhlcvDataModel(BaseModel):
-    Open: str = Field(..., description="Open price")
-    High: str = Field(..., description="High price")
-    Low: str = Field(..., description="Low price")
-    Close: str = Field(..., description="Close price")
-    Volume: str = Field(..., description="Volume")
+# class OhlcvDataModel(BaseModel):
+#     Open: str = Field(..., description="Open price")
+#     High: str = Field(..., description="High price")
+#     Low: str = Field(..., description="Low price")
+#     Close: str = Field(..., description="Close price")
+#     Volume: str = Field(..., description="Volume")
